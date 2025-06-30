6001e70e004a03e813c09d15ea03fed82e667707
ananya1188 <ananya.strix@gmail.com>
2025-06-30 16:40:06 +0530

    new changes add

--- index.php
+++ index.php
@@ -8,8 +8,13 @@
 </head>
 <body>
    
-<h1> PHP Tutorial</h1>
-<p>PHP is a server scripting language, and a powerful tool for making dynamic and interactive Web pages.
-PHP is a widely-used, free, and efficient alternative to competitors such as Microsoft's ASP. </p>
+<header id="header-section">
+    <div class="container">
+<div class="header-area">
+    <div class="logo">
+    <a href="#logo">LOGO </a>
+</div>
+</div>
+</header>
 </body>
 </html>
