09cff9944d70b6d8ac10f2615cd1b6af7b26e7a8
ananya1188 <ananya.strix@gmail.com>
2025-06-30 12:01:32 +0530

    new changes added

--- index2.php
+++ index2.php
@@ -7,7 +7,9 @@
 </head>
 <body>
    
-<h1> Hello world! hello world!</h1>
+<h1> PHP Tutorial</h1>
+<p>PHP is a server scripting language, and a powerful tool for making dynamic and interactive Web pages.
+PHP is a widely-used, free, and efficient alternative to competitors such as Microsoft's ASP. </p>
 </div>
 </body>
 </html>
