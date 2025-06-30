224703e055e95d19ab2e2ca472c14f9af09d4dcf
ananya1188 <ananya.strix@gmail.com>
2025-06-30 14:06:56 +0530

    new changes added

--- index2.php
+++ index2.php
@@ -4,13 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel ="stylesheet" href = "index.css">
+    <link rel ="stylesheet" href = "style.css">
 </head>
 <body>
    
 <h1> PHP Tutorial</h1>
 <p>PHP is a server scripting language, and a powerful tool for making dynamic and interactive Web pages.
 PHP is a widely-used, free, and efficient alternative to competitors such as Microsoft's ASP. </p>
-</div>
 </body>
 </html>
