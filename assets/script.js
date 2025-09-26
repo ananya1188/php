8d2c417b837b76c27d32a6869845b84bac7421ed
ananya1188 <ananya.strix@gmail.com>
2025-09-26 14:50:29 +0530

    changes add and remove

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,97 @@
-console.log('dd')
+//variables
+let xs  = 1;
+let y = "2";
+let z = xs +y;
+
+console.log(z);
+let a, b,c,d;
+b=2;
+c=3;
+d=5;
+let e = 2;
+const pi = 3.14;
+
+//variable scope 
+let global = 12345;
+function test(){
+console.log(global);
+
+let abc = 1223;
+console.log(abc);
+}//console.log(global);
+
+
+
+let left = "12000";
+let right = 12000;
+
+let results = left == right;
+
+console.log(results);
+
+//function
+function test(){
+    console.log("welcome");
+}
+test();
+
+ function user(name){
+console.log("hello," +name+ "!",)
+ }
+user("ananya");
+
+
+//assignment operators
+
+//simple assignment
+let g=5;
+
+//add 
+    let h= 4;
+    h +=3;
+    console.log(h);
+
+//sub
+let j=5;
+j-=4;
+console.log(j);
+
+//multiply
+let v=22;
+v *=23;
+console.log(v);
+
+//division
+let m=23;
+m /=2
+console.log(m);
+
+//modulas 
+let f=2;
+f %=31
+console.log(f); 
+
+//exponentiation
+let i =6;
+i **= 5;
+console.log(i);
+
+let l=21;
+let n="21";
+let result = l===n;
+console.log(result);
+
+
+let uv=2;
+sb=7;
+bh=23;
+wq=21;
+bj=32;
+console.log(uv,sb,bh,wq,bj);
+
+var x=24;
+ x=32;
+console.log(x);
+
+const username="ananya";
+console.log(username);
